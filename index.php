3ed55a34a8ee14af8737e3ebfe929b1c24362c73
LiliAnardaV <LiliAnardaV@users.noreply.github.com>
2016-08-22 23:05:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <html>
 <header>
+  <div id="logo">
+    
+  </div>
+  <div id="nav">
+    
+  </div>
 </header>
 <body>
+  <div id="booking">
+    
+  </div>
+  <div id="slider">
+    
+  </div>
+  
 </body>
 <footer>
 </footer>
